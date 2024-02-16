6dc56e474a36a58ee5b8cb07979989b008311fa9
Aiwinraj1810 <aiwinraj1810@gmail.com>
2024-02-16 10:12:18 +0530

    Password-strength style changed

--- src/components/PasswordGen.js
+++ src/components/PasswordGen.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import '../styles/PasswordGen.css'
+import styled from 'styled-components';
+
+
+
+
 function generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSpecialChars) {
   let charset = '';
   if (includeLowerCase) charset += 'abcdefghijklmnopqrstuvwxyz';
@@ -11,21 +16,36 @@ function generatePassword(length, includeLowerCase, includeUpperCase, includeNum
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset[randomIndex];
+    console.log(charset)
   }
   return password;
 }
 
 function getPasswordStrengthLabel(length) {
-    if (length >= 8 && length <= 10) {
+    if (length >= 4 && length <= 8) {
       return 'Weak';
-    } else if (length >= 11 && length <= 14) {
+    } else if (length >= 8 && length <= 24) {
       return 'Strong';
-    } else if (length >= 15) {
+    } else if (length >= 25) {
       return 'Excellent';
     } else {
       return 'Weak';
     }
   }
+  const PasswordStrengthLabel = styled.span`
+  color: ${props => {
+    switch (props.strength) {
+      case 'Weak':
+        return 'red';
+      case 'Strong':
+        return 'rgb(105, 179, 88)';
+      case 'Excellent':
+        return 'rgb(26, 142, 0)';
+      default:
+        return 'black';
+    }
+  }};
+`;
   
 
 
@@ -80,42 +100,46 @@ function PasswordGenerator() {
     alert("Password copied to clipboard!");
   };
   const passwordStrengthLabel = getPasswordStrengthLabel(generatedPassword.length);
+  const strengthLabel = getPasswordStrengthLabel(passwordLength);
   return (
 
-    <div>
+    <div className='main-wrapper'>
 
     <div className='wrapper'>
 
     <div>
       <h1>Password Generator</h1>
-        <h2>Generate complex passwords to be safe</h2>
+        <span className='description'>Generate complex passwords to be safe</span>
       <div className='password-area' style={{textAlign:'left', display:'flex'}}>
       <div>
-          <h3 style={{margin:0, marginRight:10}}>Generated Password:</h3>
+          <h3 style={{margin:0, marginRight:10}}>Generated Password : </h3>
          
         </div>
       <textarea value={generatedPassword} onChange={handlePasswordChange} />
-      <button disabled={!generatedPassword} onClick={handleCopyPassword}><i class="fa-solid fa-copy"></i> Copy</button>
+     
+
+      <button disabled={!generatedPassword} onClick={handleCopyPassword}><i className="fa-solid fa-copy"></i> Copy</button>
+  
       </div>
       <div>
       {/* Existing code for displaying generated password */}
       {generatedPassword && (
-        <div>
-          <span>Password Strength: </span>
-          <span>{passwordStrengthLabel}</span>
-          {/* Existing code... */}
+        <div className='strength' style={{display:'flex'}}>
+          <span>Password Strength : </span>
+          <div><PasswordStrengthLabel strength={strengthLabel}>{strengthLabel}</PasswordStrengthLabel> </div> 
+          
         </div>
       )}
     </div>
       <div className='main-section' style={{textAlign:'left'}}>
-        <label >Password Length: {passwordLength}</label><br />
+        <label >Password Length :  {passwordLength}</label><br />
         <input className='slider' type="range" min="4" max="32" value={passwordLength} onChange={handleLengthChange} />
       </div>
       <div className='criterias'>
 
       <div >
         <label>
-          <input type="checkbox" checked={includeLowerCase} onChange={handleLowerCaseChange} />
+        <input type="checkbox" checked={includeLowerCase} onChange={handleLowerCaseChange} style={{ color: 'aqua' }} />
           Include Lowercase Characters
         </label>
       </div>
@@ -133,12 +157,15 @@ function PasswordGenerator() {
       </div>
       <div>
         <label>
-          <input type="checkbox" checked={includeSpecialChars} onChange={handleSpecialCharsChange} />
+          <input type="checkbox" checked={includeSpecialChars} onChange={handleSpecialCharsChange} style={{color:'aqua'}} />
           Include Special Characters
         </label>
       </div>
       </div>
+      <div className='btn-space'>
+
       <button className='generate' onClick={handleGeneratePassword}>Generate Password</button>
+      </div>
 
     </div>
     </div>
